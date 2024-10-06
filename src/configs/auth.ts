165576e4fa590f8e2e5ddaa73945ad833b8de7e1
Chat Book <chatbookai@gmail.com>
2024-10-05 18:00:59 -0700

    1986

--- src/configs/auth.ts
+++ src/configs/auth.ts
@@ -1,5 +1,5 @@
 export default {
-  backEndApi: 'https://api.chivesweave.org:1986',
+  backEndApi: 'https://api.chivesweave.org',
   faucetApi: 'https://faucet.chivesweave.org/faucet.php',
   meEndpoint: '/auth/me',
   loginEndpoint: '/jwt/login',
